60008db3ced375336833a4d250aeb1d641a85c16
Khaled Mokhtar <khaledmokhtar@Khaleds-MBP.lan>
2026-06-15 05:22:51 +0400

    remove email provider - fix auth build

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,6 @@
 import { NextAuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import GitHubProvider from 'next-auth/providers/github'
-import EmailProvider from 'next-auth/providers/email'
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import { prisma } from '@/lib/prisma'
 
@@ -15,10 +14,6 @@ export const authOptions: NextAuthOptions = {
     GitHubProvider({
       clientId:     process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
-    }),
-    EmailProvider({
-      server: process.env.EMAIL_SERVER,
-      from:   process.env.EMAIL_FROM,
     }),
   ],
   pages: {
